588b1d85669fbeb1eb8e7f4747df2b35342b392c
Uzor Efedigbue <cefedigbuej@gmail.com>
2020-09-10 19:59:48 +0100

    removed comment from routes files

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,4 @@ Rails.application.routes.draw do
   get '/cna_solar', to: 'cnahomes#solar_power'
   
   root 'cnahomes#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
