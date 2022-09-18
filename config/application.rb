6f221f7e7d19afb234e37c86016bad4dea919c95
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-09-18 18:55:47 +0100

    upgraded app to rails 7

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CnaBuildingSolutions
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.active_record.legacy_connection_handling = false
 
     # Configuration for the application, engines, and railties goes here.
     #
